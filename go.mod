1e0e46c6b141e59617792aac4d2c906763df15cd
Ade Slade <ade@gamebench.net>
2019-11-13 17:08:45 +0000

    Basic command implementation

--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
 module github.com/GameBench/gba-client
 
-require github.com/GameBench/gba-client-go v0.0.0-20191113103443-a6db0fbcaed8 // indirect
+require (
+	github.com/GameBench/gba-client-go v0.0.0-20191113103443-a6db0fbcaed8 // indirect
+	github.com/spf13/cobra v0.0.5 // indirect
+)
--- main.go
+++ main.go
@@ -1,39 +1,168 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 
 	"github.com/GameBench/gba-client-go"
+	"github.com/spf13/cobra"
 )
 
-func main() {
-	config := &gba.Config{BaseUrl: "http://localhost:8000"}
-	client := gba.New(config)
+var (
+	server string
+	username string
+	password string
+	client *gba.GbaClient
+)
 
-	listDevicesCommand := flag.NewFlagSet("list-devices", flag.ExitOnError)
-
-	if len(os.Args) < 2 {
-		os.Exit(1)
-	}
-
-	switch os.Args[1] {
-	case "list-devices":
-		listDevicesCommand.Parse(os.Args[2:])
-	default:
-		flag.PrintDefaults()
-		os.Exit(1)
-	}
-
-	if listDevicesCommand.Parsed() {
+var rootCmd = &cobra.Command{
+	Use:   "foo",
+	Short: "Foo",
+	Long:  "Foo",
+	PersistentPreRun: func(cmd *cobra.Command, args[]string) {
+		config := &gba.Config{BaseUrl: server, Username: username, Password: password}
+		client = gba.New(config)
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Foo")
 		devices, err := client.ListDevices()
 		if err != nil {
 			panic(err)
 		}
 
-		for _, device := range devices {
-			fmt.Printf("%s\t%s\n", device.Id, device.Name)
+		fmt.Println(devices)
+	},
+}
+
+var listDevicesCmd = &cobra.Command{
+	Use: "list-devices",
+	Short: "list-devices",
+	Long: "list-devices",
+	Run: func(cmd *cobra.Command, args []string) {
+		devices, err := client.ListDevices()
+		if err != nil {
+			panic(err)
 		}
+
+		fmt.Println(devices)
+	},
+}
+
+var listDeviceAppsCmd = &cobra.Command{
+	Use: "list-device-apps [DEVICE ID]",
+	Short: "list-device-apps",
+	Long: "list-device-apps",
+	Args: cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		devices, err := client.GetDeviceApps(args[0])
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(devices)
+	},
+}
+
+var getDeviceCmd = &cobra.Command{
+	Use: "get-device [DEVICE ID]",
+	Short: "get-device",
+	Long: "get-device",
+	Args: cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		devices, err := client.GetDevice(args[0])
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(devices)
+	},
+}
+
+var startSessionCmd = &cobra.Command{
+	Use: "start-session [DEVICE ID] [APP ID]",
+	Short: "start-session",
+	Long: "start-session",
+	Args: cobra.ExactArgs(2),
+	Run: func(cmd *cobra.Command, args []string) {
+		devices, err := client.StartSession(args[0], args[1], nil)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(devices)
+	},
+}
+
+var stopSessionCmd = &cobra.Command{
+	Use: "stop-session [SESSION ID]",
+	Short: "stop-session",
+	Long: "stop-session",
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		err := client.StopSession(args[0])
+		if err != nil {
+			panic(err)
+		}
+	},
+}
+
+var syncSessionsCmd = &cobra.Command{
+	Use: "sync-sessions",
+	Short: "sync-sessions",
+	Long: "sync-sessions",
+	Run: func(cmd *cobra.Command, args []string) {
+		err := client.Sync()
+		if err != nil {
+			panic(err)
+		}
+	},
+}
+
+var getPropertiesCmd = &cobra.Command{
+	Use: "get-properties",
+	Short: "get-properties",
+	Long: "get-properties",
+	Run: func(cmd *cobra.Command, args []string) {
+		properties, err := client.GetProperties()
+		if err != nil {
+			panic(err)
+		}
+
+		for key, value := range properties {
+			fmt.Println(key, value)
+		}
+
+	},
+}
+
+var setPropertyCmd = &cobra.Command{
+	Use: "set-property",
+	Short: "set-property",
+	Long: "set-property",
+	Run: func(cmd *cobra.Command, args []string) {
+		err := client.SetProperty(args[0], args[1])
+		if err != nil {
+			panic(err)
+		}
+	},
+}
+
+func main() {
+	rootCmd.AddCommand(listDevicesCmd)
+	rootCmd.AddCommand(listDeviceAppsCmd)
+	rootCmd.AddCommand(getDeviceCmd)
+	rootCmd.AddCommand(startSessionCmd)
+	rootCmd.AddCommand(stopSessionCmd)
+	rootCmd.AddCommand(syncSessionsCmd)
+	rootCmd.AddCommand(getPropertiesCmd)
+	rootCmd.AddCommand(setPropertyCmd)
+
+	rootCmd.PersistentFlags().StringVarP(&server, "server", "s", "", "")
+	rootCmd.PersistentFlags().StringVarP(&username, "username", "u", "", "")
+	rootCmd.PersistentFlags().StringVarP(&password, "password", "p", "", "")
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 }
